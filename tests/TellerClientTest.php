90f04ae250b56434063ca7d28d27533fa1bf2707
Levi Zoesch <lzoesch@gmail.com>
2023-10-21 19:13:50 -0700

    Update TellerClientTest.php
    Test exception is thrown.

--- tests/TellerClientTest.php
+++ tests/TellerClientTest.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace LeviZoesch\TellerSDK\Tests;
 use Illuminate\Support\Facades\Date;
-
+use LeviZoesch\TellerSDK\Exceptions\MissingTellerConfigurationException;
 
 class TellerClientTest extends TestCase
 {
-
     public function testPass()
     {
         $variable = true;
@@ -18,10 +16,15 @@ class TellerClientTest extends TestCase
 
         $this->assertTrue($variable, 'The value is not true.');
     }
-
     public function testTellerTestTokenIsDefined()
     {
         $this->assertNull(config('teller.TEST_TOKEN'), 'The teller test token is missing from the environment file, or undefined.');
+    }
+
+    public function testMissingTellerConfigurationExceptionThrown() {
+        $this->expectException(MissingTellerConfigurationException::class);
+
+        throw new MissingTellerConfigurationException();
     }
 
 }
