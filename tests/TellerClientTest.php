870684df447c97e7bf1e88d88a20f524a28f8f1b
Levi Zoesch <lzoesch@gmail.com>
2023-10-21 22:31:26 -0700

    Test

--- tests/TellerClientTest.php
+++ tests/TellerClientTest.php
@@ -1,22 +1,31 @@
 <?php
 
 namespace LeviZoesch\TellerSDK\Tests;
-use Illuminate\Support\Facades\Date;
 use LeviZoesch\TellerSDK\Exceptions\MissingTellerConfigurationException;
+use LeviZoesch\TellerSDK\TellerClient;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class TellerClientTest extends TestCase
 {
-    public function testPass()
+    /** @var TellerClient|MockObject */
+    private $tellerClient;
+
+    protected function setUp(): void
     {
-        $variable = true;
-
-        if (Date::now()->toString() === '01/01/2000') {
-            $variable = false;
-        }
-
-        $this->assertTrue($variable, 'The value is not true.');
+        $this->tellerClient = $this->getMockBuilder(TellerClient::class)
+            ->disableOriginalConstructor()
+            ->getMock();
     }
-    public function testTellerTestTokenIsDefined()
+
+    public function testListAccounts()
+    {
+        $teller = new TellerClient(config('teller.TEST_TOKEN'));
+        $result = $teller->listAccounts();
+        $this->assertJson($result);
+
+    }
+
+    public function testTellerTestTokenIsNotDefined()
     {
         $this->assertNull(config('teller.TEST_TOKEN'), 'The teller test token is missing from the environment file, or undefined.');
     }
